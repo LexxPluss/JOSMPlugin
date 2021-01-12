7c28ccb488f34f5c37d27371ff5938cea0d29f8e
Yuichi.Imamura <yuichi.imamura@lexxpluss.com>
2021-01-12 13:59:56 +0900

    First

---
+++ LexxPlussExporter/src/org/openstreetmap/josm/plugins/lexxpluss/LexxPlussPlugin.java
@@ -0,0 +1,21 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.lexxpluss;
+
+import org.openstreetmap.josm.actions.ExtensionFileFilter;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.plugins.lexxpluss.io.LexxPlussExporter;
+
+/**
+ * 
+ * LexxPluss Plugin
+ * @author LexxPluss
+ *
+ */
+public class LexxPlussPlugin extends Plugin {
+
+    public LexxPlussPlugin(PluginInformation info) {
+        super(info);
+        ExtensionFileFilter.addExporter(new LexxPlussExporter());
+    }
+}
---
+++ LexxPlussExporter/src/org/openstreetmap/josm/plugins/lexxpluss/io/LexxPlussExporter.java
@@ -0,0 +1,66 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.lexxpluss.io;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.openstreetmap.josm.actions.ExtensionFileFilter;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.gui.io.importexport.OsmExporter;
+import org.openstreetmap.josm.gui.progress.NullProgressMonitor;
+import org.openstreetmap.josm.gui.progress.ProgressMonitor;
+import org.openstreetmap.josm.io.IllegalDataException;
+import org.openstreetmap.josm.io.CachedFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.InvalidPathException;
+import java.text.MessageFormat;
+
+import javax.swing.JOptionPane;
+
+import org.openstreetmap.josm.actions.ExtensionFileFilter;
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.layer.Layer;
+import org.openstreetmap.josm.gui.layer.OsmDataLayer;
+import org.openstreetmap.josm.io.Compression;
+import org.openstreetmap.josm.io.OsmWriter;
+import org.openstreetmap.josm.io.OsmWriterFactory;
+import org.openstreetmap.josm.spi.preferences.Config;
+import org.openstreetmap.josm.tools.Logging;
+import org.openstreetmap.josm.tools.Utils;
+
+/**
+ * OSM Exporter for o5n format (*.osn).
+ * @author LexxPluss
+ *
+ */
+public class LexxPlussExporter extends OsmExporter {
+    /**
+     * File extension.
+     */
+    private static final String EXTENSION = "osn";
+    
+    public LexxPlussExporter() {
+        super(new ExtensionFileFilter(EXTENSION, EXTENSION, 
+                tr("OSM Server Files LexxPluss format") + " (*."+EXTENSION+")"));
+    }
+
+    @Override
+    protected void doSave(File file, OsmDataLayer layer) throws IOException {
+
+        //
+        // TODO Layer Deepcopy And Convert
+        //
+        System.out.println("LexxPluss Plugin doSave");
+        super.doSave(file, layer);
+    }
+}
