800ea02e594bd4629873a8e14ca406ff511f81a8
Atsunori Saito <sai@yedo.com>
2024-05-23 17:09:18 +0900

    feat: Add SFTP functionality to plugin
    The update introduces SFTP capabilities to the plugin. It includes a new dialog for handling open/save settings via SFTP, and methods for downloading and opening files from an SFTP server. The code also provides a way to save these settings for future use. A new class has been added to manage tool settings, which includes getting and setting host, user, password, and OSM path information.

--- lexxpluss_tools/src/org/openstreetmap/josm/plugins/lexxpluss/SftpAction.java
+++ lexxpluss_tools/src/org/openstreetmap/josm/plugins/lexxpluss/SftpAction.java
@@ -6,23 +6,117 @@
 
 package org.openstreetmap.josm.plugins.lexxpluss;
 
+import java.io.File;
+import java.util.Arrays;
 import java.awt.event.ActionEvent;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.SftpException;
+import com.jcraft.jsch.UserInfo;
 import org.openstreetmap.josm.actions.JosmAction;
+import org.openstreetmap.josm.actions.OpenFileAction;
+import org.openstreetmap.josm.gui.MainApplication;
 
+/**
+ * Action for opening/saving using sftp.
+ */
 public class SftpAction extends JosmAction {
 
+    /**
+     * Constructs a new {@code SftpAction}.
+     */
     public SftpAction() {
-        super("SFTP", "mapmode/sftpaction", "SFTP", null, false);
+        super("Open/Save using sftp...", "mapmode/sftpaction", "Open/Save using sftp", null, false);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         var dialog = new SftpDialog();
         dialog.showDialog();
+        var value = dialog.getValue();
+        System.out.println("Value: " + value);
+        if (value != 3)
+            dialog.saveSettings();
+        if (value == 1)
+            download();
     }
 
     @Override
     protected void updateEnabledState() {
         setEnabled(true);
+    }
+
+    /**
+     * Downloads the osm file.
+     */
+    private void download() {
+        try {
+            var jsch = new JSch();
+            var session = jsch.getSession(
+                    ToolsSettings.getUser(),
+                    ToolsSettings.getHost()
+            );
+            session.setUserInfo(new UserInfoImpl());
+            session.connect();
+            var channel = (ChannelSftp)session.openChannel("sftp");
+            channel.get(ToolsSettings.getOsmPath(), "/tmp/a.osm", null, ChannelSftp.OVERWRITE);
+            open();
+        } catch (JSchException | SftpException e) {
+            System.err.println("JSchException: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Opens the downloaded osm file.
+     */
+    private void open() {
+        var files = new File[]{new File("/tmp/a.osm")};
+        var task = new OpenFileAction.OpenFileTask(Arrays.asList(files), null);
+        MainApplication.worker.submit(task);
+    }
+
+    /**
+     * Implementation of UserInfo.
+     */
+    private static class UserInfoImpl implements UserInfo {
+
+        /**
+         * Constructs a new {@code UserInfoImpl}.
+         */
+        public UserInfoImpl() {
+            super();
+        }
+
+        @Override
+        public String getPassphrase() {
+            return "";
+        }
+
+        @Override
+        public String getPassword() {
+            return ToolsSettings.getPassword();
+        }
+
+        @Override
+        public boolean promptPassword(String s) {
+            return false;
+        }
+
+        @Override
+        public boolean promptPassphrase(String s) {
+            return false;
+        }
+
+        @Override
+        public boolean promptYesNo(String s) {
+            return false;
+        }
+
+        @Override
+        public void showMessage(String s) {
+            System.out.println("Message: " + s);
+        }
     }
 }
--- lexxpluss_tools/src/org/openstreetmap/josm/plugins/lexxpluss/SftpDialog.java
+++ lexxpluss_tools/src/org/openstreetmap/josm/plugins/lexxpluss/SftpDialog.java
@@ -18,17 +18,41 @@ import org.openstreetmap.josm.gui.ExtendedDialog;
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.tools.GBC;
 
+/**
+ * Dialog for sftp open/save settings.
+ */
 public class SftpDialog extends ExtendedDialog {
 
+    /**
+     * Columns width.
+     */
     private static int columns = 40;
-    private JTextField hostName = new JTextField(null, null, columns);
-    private JTextField userName = new JTextField(null, "lexxpluss", columns);
-    private JPasswordField password = new JPasswordField();
-    private JTextField osmPath = new JTextField(null, "/home/lexxpluss/osm/map.osm", columns);
 
+    /**
+     * Host name field.
+     */
+    private JTextField hostName = new JTextField(null, ToolsSettings.getHost(), columns);
+
+    /**
+     * User name field.
+     */
+    private JTextField userName = new JTextField(null, ToolsSettings.getUser(), columns);
+
+    /**
+     * Password field.
+     */
+    private JPasswordField password = new JPasswordField(ToolsSettings.getPassword());
+
+    /**
+     * Osm path field.
+     */
+    private JTextField osmPath = new JTextField(null, ToolsSettings.getOsmPath(), columns);
+
+    /**
+     * Constructs a new {@code SftpDialog}.
+     */
     public SftpDialog() {
-        super(MainApplication.getMainFrame(), "SFTP", new String[] {"Download", "Upload", "Cancel"}, true);
-//        setButtonIcons(new String[] {"ok", "cancel"});
+        super(MainApplication.getMainFrame(), "Open/Save using sftp", new String[] {"Open", "Save", "Cancel"}, true);
         contentInsets = new Insets(15, 15, 5, 15);
         var panel = new JPanel(new GridBagLayout());
         addLabelled(panel, "Hostname:", hostName);
@@ -39,6 +63,26 @@ public class SftpDialog extends ExtendedDialog {
         setDefaultButton(3);
     }
 
+    /**
+     * Save settings.
+     */
+    public void saveSettings() {
+        System.out.println("Host: " + hostName.getText());
+        System.out.println("User: " + userName.getText());
+        System.out.println("Password: " + new String(password.getPassword()));
+        System.out.println("Osm path: " + osmPath.getText());
+        ToolsSettings.setHost(hostName.getText());
+        ToolsSettings.setUser(userName.getText());
+        ToolsSettings.setPassword(new String(password.getPassword()));
+        ToolsSettings.setOsmPath(osmPath.getText());
+    }
+
+    /**
+     * Add labelled component
+     * @param panel panel
+     * @param str label
+     * @param c component
+     */
     private void addLabelled(JPanel panel, String str, Component c) {
         var label = new JLabel(str);
         panel.add(label, GBC.std());
